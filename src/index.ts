d3c58f6f29cc57a01ffaaac249cf0ec8e961137f
0x1luxa <0x1luxa@gmail.com>
2025-03-19 17:08:17 +0400

    Reorganize and uncomment export statements in index.ts to enhance clarity and improve documentation of module exports.

--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,27 @@ export * from "./utils/web3"
 export * from "./constants"
 
 // Re-export commonly used functions for better documentation
-// export {
-//   CHAIN_IDS_BY_NAME,
-//   CHAIN_NAMES_BY_ID,
-//   EIDS_BY_CHAIN_ID,
-//   SCAN_ADDRESS_PATH_BY_CHAIN_ID,
-//   SCAN_LINK_BY_CHAIN_ID,
-//   SCAN_TX_PATH_BY_CHAIN_ID,
-// } from "./constants"
-// export {
-//   capitalize,
-//   cropValue,
-//   formatAmount,
-//   formatPercentValue,
-//   formatUsdValue,
-//   sequentialActionHandler,
-//   shortenString,
-//   wait,
-// } from "./utils/common"
-// export { getDaysAgo, getFromNow } from "./utils/date"
-// export {
-//   getScanLink,
-//   parseRpcError,
-//   shortenAddress,
-// } from "./utils/web3"
+export {
+  CHAIN_IDS_BY_NAME,
+  CHAIN_NAMES_BY_ID,
+  EIDS_BY_CHAIN_ID,
+  SCAN_ADDRESS_PATH_BY_CHAIN_ID,
+  SCAN_LINK_BY_CHAIN_ID,
+  SCAN_TX_PATH_BY_CHAIN_ID,
+} from "./constants"
+export {
+  capitalize,
+  cropValue,
+  formatAmount,
+  formatPercentValue,
+  formatUsdValue,
+  sequentialActionHandler,
+  shortenString,
+  wait,
+} from "./utils/common"
+export { getDaysAgo, getFromNow } from "./utils/date"
+export {
+  getScanLink,
+  parseRpcError,
+  shortenAddress,
+} from "./utils/web3"
