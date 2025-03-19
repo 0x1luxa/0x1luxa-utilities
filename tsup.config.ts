fdf1b8f340d9a89c6ab6ea49c83482f8f1a1e963
0x1luxa <0x1luxa@gmail.com>
2025-03-19 19:18:35 +0400

    Refactor package.json, tsconfig.json, and tsup.config.ts for improved module handling and build configuration. Update output directory in tsconfig, adjust entry format in tsup, and enhance exports for better compatibility.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from "tsup"
+import { defineConfig, Options } from "tsup"
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-})
+export default defineConfig((options: Options) => ({
+  entry: {
+    index: "src/index.ts",
+  },
+
+  format: ["esm"],
+  esbuildOptions(options) {
+    options.banner = {
+      js: '"use client"',
+    }
+  },
+  dts: true,
+  minify: true,
+  external: ["react"],
+  ...options,
+}))
