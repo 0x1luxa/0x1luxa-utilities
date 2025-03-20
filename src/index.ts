6a855f9dc99a00b077ef835cf74be7c31ffdb82b
0x1luxa <0x1luxa@gmail.com>
2025-03-20 11:21:52 +0400

    Enhance package.json with exports for utility modules and update build script to use TypeScript compiler. Modify tsconfig.json to enable declaration files and adjust paths for module resolution. Refactor src/index.ts to streamline exports.

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,5 @@
-// utils
+// src/index.ts
+export * from "./constants"
 export * from "./utils/common"
 export * from "./utils/date"
 export * from "./utils/web3"
-
-// constants
-export * from "./constants"
-
-// Re-export commonly used functions for better documentation
-export {
-  CHAIN_IDS_BY_NAME,
-  CHAIN_NAMES_BY_ID,
-  EIDS_BY_CHAIN_ID,
-  SCAN_ADDRESS_PATH_BY_CHAIN_ID,
-  SCAN_LINK_BY_CHAIN_ID,
-  SCAN_TX_PATH_BY_CHAIN_ID,
-} from "./constants"
-export {
-  capitalize,
-  cropValue,
-  formatAmount,
-  formatPercentValue,
-  formatUsdValue,
-  sequentialActionHandler,
-  shortenString,
-  wait,
-} from "./utils/common"
-export { getDaysAgo, getFromNow } from "./utils/date"
-export {
-  getScanLink,
-  parseRpcError,
-  shortenAddress,
-} from "./utils/web3"
